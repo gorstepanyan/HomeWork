caf646e36b4c2d7a90cc607454aa92ea6f780e86
Gor Stepanyan <gor.stepanyan69@gmail.com>
2023-03-09 19:19:31 +0400

    20230308_1

--- Lesson21/MyClass.cs
+++ Lesson21/MyClass.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Lesson21
+﻿namespace Lesson21
 {
-    internal class Class
+    internal class MyClass
     {
+        public string Name { get; set; }
+        private string SurName { get; set; }
+        internal string Car { get; set; }
+        protected string Food { get; set; }
+        protected internal string FoodName { get; set; }
+        private protected string CarName { get; set; }
+
+    }
+
+
+    internal class MyClass1 : MyClass
+    {
+        internal MyClass1()
+        {
+            FoodName = string.Empty;
+            Food = string.Empty;
+            CarName = string.Empty;
+
+        }
     }
 }
--- Lesson21/Program.cs
+++ Lesson21/Program.cs
@@ -4,10 +4,14 @@
     {
         static void Main(string[] args)
         {
-            int num = 1;
+            //int num = 1;
             //Rec(num);
-            Console.WriteLine(RecSum(num));
+            //Console.WriteLine(RecSum(num));
 
+            var myClass = new MyClass();
+            Console.WriteLine(myClass.Name);//From Public
+            Console.WriteLine(myClass.FoodName);//From Protected Internal
+            Console.WriteLine(myClass.Car);//From Internal
         }
 
         static void Rec(int num)
