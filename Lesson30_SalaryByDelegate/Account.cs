c20afa0c28204bd7b1f8bc95dd12d8668db4ebf6
Gor Stepanyan <gor.stepanyan69@gmail.com>
2023-03-20 23:39:23 +0400

    Delegate

---
+++ Lesson30_SalaryByDelegate/Account.cs
@@ -0,0 +1,38 @@
+﻿namespace Lesson30_Delegate
+{
+    delegate void AccountHandler(string message);
+    internal class Account
+    {
+        int _sum;
+        public Account(int sum)
+        {
+            _sum = sum;
+        }
+
+        AccountHandler? takenHandler;
+        public void RegisterHandler(AccountHandler handler)
+        {
+            takenHandler += handler;
+        }
+        public void UnRegisterHandler(AccountHandler handler)
+        {
+            takenHandler -= handler;
+        }
+
+
+        public void Taken(int sum)
+        {
+            if (_sum > sum)
+            {
+                _sum -= sum;
+                takenHandler?.Invoke($"Debited from account {sum} y.e.");
+            }
+            else
+            {
+                takenHandler?.Invoke($"Insufficient funds. Balance: {_sum} y.e.");
+            }
+        }
+
+
+    }
+}
---
+++ Lesson30_SalaryByDelegate/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Lesson30_Delegate
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Account account = new Account(200);
+            account.RegisterHandler(PrintSimpleMessage);
+            account.RegisterHandler(PrintColorMessage);
+            //account.UnRegisterHandler(PrintSimpleMessage);
+            account.Taken(100);
+            account.Taken(150);
+
+            account.UnRegisterHandler(PrintColorMessage);
+            account.Taken(50);
+        }
+
+        static void PrintSimpleMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+        static void PrintColorMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
