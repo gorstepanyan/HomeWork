68dfd2604e1fdf5b9b9c407189c504797a011e59
Gor Stepanyan <gor.stepanyan69@gmail.com>
2023-03-23 02:46:13 +0400

    Calculator

---
+++ Lesson32_WPF_Calculator/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Lesson32_WPF_Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach (UIElement el in MainRoot.Children)
+            {
+                if (el is Button)
+                {
+                    ((Button)el).Click += Button_Click;
+                }
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+            if (str == "AC")
+            {
+                textLabel.Text = string.Empty;
+            }
+            else if (str == "=")
+            {
+                string value = new DataTable().Compute(textLabel.Text, null).ToString();
+                textLabel.Text = value;
+            }
+            else
+            {
+                textLabel.Text += str;
+            }
+        }
+    }
+}
